749d11067e3953424072a22835e6cf71f4d98b63
Kristian Welsh <kris@Apple-User.local>
2015-06-05 23:38:54 +0100

    Added incremental vertical line drawing
    It draws the lines in 40px segments because when i have a maze
    data structure, i can lookup the current line being drawn and
    choose to not draw it if it is a hole in the wall.
    
    Due to nexted loops this current method is O(n^2) possibly
    replace this in the future with a single loop and modular
    arithmatic to change horizontal position.

--- src/maze.js
+++ src/maze.js
@@ -1,4 +1,27 @@
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-context.fillStyle = "#DDDDDD";
-context.fillRect(0, 0, 400, 400);
+doIt();
+
+function doIt() {
+  setupCanvas();
+  drawGrid();
+}
+
+function setupCanvas() {
+  canvas = document.getElementById("myCanvas");
+  context = canvas.getContext("2d");
+  context.fillStyle = "#DDDDDD";
+  context.fillRect(0, 0, 400, 400);
+}
+
+function drawGrid() {
+  drawVertical();
+  context.stroke();
+}
+
+function drawVertical() {
+  for (var i = 0; i <= 10; i++) {
+    context.moveTo(40 * i, 0);
+    for (var j = 0; j <= 10; j++) {
+      context.lineTo(40 * i, 40 * j);
+    }
+  }
+}
