fbe840edcdfff04e5d656c1fd2d9bc66c66c3738
Kristian Welsh <kriswelsh@gmail.com>
2015-06-29 20:47:37 +0100

    Removed current maze drawer as it will not be used
    The current maze drawin system was designed in about 1 hour,
    and it was made before i had decided how to generate the maze.
    
    The idea was that i would have an array of true and false values,
    true if there was a wall there, false if there wasn't.
    I would then have tried to draw every possible wall but first
    check if it was supposed to be there from the array.
    This seems inefficient in light of my new array of wall positions
    And the new system to replace it would be as simple as itterating
    throught the points and drawing a wall at each.

--- src/maze.js
+++ src/maze.js
@@ -5,76 +5,6 @@ NUM_COLUMNS = 10;
 TILE_WIDTH = CANVAS_WIDTH / NUM_ROWS;
 TILE_HEIGHT = CANVAS_HEIGHT / NUM_COLUMNS;
 
-LineDrawer = function(context) {
-  this.draw = function(xx, yy, numWalls, isVertical, offset) {
-    context.moveTo(xx, yy);
-    var func = function(curCall) {
-      drawWall(xx, yy, offset * curCall, isVertical)
-    }
-    times(numWalls, func);
-  }
-
-  function drawWall(xx, yy, newVal, isVertical) {
-    (isVertical) ? yy = newVal : xx = newVal;
-    context.lineTo(xx, yy);
-  }
-}
-
-VerticalDrawer = function(context) {
-  var lineDrawer = new LineDrawer(context);
-  
-  this.draw = function() {
-    drawMultipleLines(NUM_ROWS + 1, TILE_WIDTH);
-  }
-
-  function drawMultipleLines(numLines, offset) {
-    for (var curLine = 0; curLine < numLines; curLine++)
-      drawLine(offset * curLine);
-  }
-
-  function drawLine(startingX) {
-    lineDrawer.draw(startingX, 0, NUM_ROWS + 1, true, TILE_HEIGHT);
-  }
-}
-
-HorizontalDrawer = function(context) {
-  var lineDrawer = new LineDrawer(context);
-  
-  this.draw = function() {
-    drawMultipleLines(NUM_COLUMNS + 1, TILE_HEIGHT);
-  }
-
-  function drawMultipleLines(numLines, offset) {
-    for (var curLine = 0; curLine < numLines; curLine++)
-      drawLine(offset * curLine);
-  }
-
-  function drawLine(startingY) {
-    lineDrawer.draw(0, startingY, NUM_COLUMNS + 1, false, TILE_WIDTH);
-  }
-}
-
-Drawer = function(context) {
-  this.drawGrid = function() {
-    drawVerticalLines();
-    drawHorizontalLines();
-    context.stroke();
-  }
-
-  function drawVerticalLines() {
-    new VerticalDrawer(context).draw();
-  }
-
-  function drawHorizontalLines() {
-    new HorizontalDrawer(context).draw();
-  }
-}
-
-function times(numCalls, func) {
-  for (var curCall = 0; curCall < numCalls; curCall++)
-    func(curCall);
-}
-
 Point = function(x, y) {
   var x = x;
   var y = y;
@@ -194,7 +124,6 @@ Main = function() {
     var context = retrieveCanvasContext();
     fillBackground(context);
     new Maze().create(new Point(4, 9));
-    new Drawer(context).drawGrid();
   }
 
   function retrieveCanvasContext() {
