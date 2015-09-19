ed1bfadda0bac27882b7a40590f7e5b96573caa3
Kristian Welsh <kriswelsh@gmail.com>
2015-09-19 11:20:31 +0100

    extracted Point to Point.js using require.js

---
+++ www/src/Point.js
@@ -0,0 +1,31 @@
+define(function(require) {
+  Point = function(x, y) {
+    var x = x;
+    var y = y;
+
+    this.plus = function (addX, addY) {
+      return new Point(x + addX, y + addY);
+    }
+
+    this.getX = function() {
+      return x;
+    }
+
+    this.getY = function() {
+      return y;
+    }
+
+    this.add = function(pointToAdd) {
+      if(arguments.length === 1)
+        return new Point(arguments[0].getX() + this.getX(), arguments[0].getY() + this.getY());
+      else if(arguments.length === 2)
+        return new Point(arguments[0] + this.getX(), arguments[1] + this.getY());
+      throw new Error("unexpected arguments in Point::add");
+    }
+
+    this.toString = function() {
+      return "Point ".concat("x: ", x, ", y: ", y);
+    }
+  }
+  return Point;
+});
--- www/src/main.js
+++ www/src/main.js
@@ -37,35 +37,7 @@ define(function(require) {
     return Math.round(Math.random() * (upperBound - lowerBound)) + lowerBound;
   }
 
-  Point = function(x, y) {
-    var x = x;
-    var y = y;
-
-    this.plus = function (addX, addY) {
-      return new Point(x + addX, y + addY);
-    }
-
-    this.getX = function() {
-      return x;
-    }
-
-    this.getY = function() {
-      return y;
-    }
-
-    this.add = function(pointToAdd) {
-      if(arguments.length === 1)
-        return new Point(arguments[0].getX() + this.getX(), arguments[0].getY() + this.getY());
-      else if(arguments.length === 2)
-        return new Point(arguments[0] + this.getX(), arguments[1] + this.getY());
-      throw new Error("unexpected arguments in Point::add");
-    }
-
-    this.toString = function() {
-      return "Point ".concat("x: ", x, ", y: ", y);
-    }
-
-  }
+  Point = require("./Point.js");
 
   // 0 = unvisited
   // 1 = visited
